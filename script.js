230ae8ad0cc3dccf9cea76398292faf253e5e485
Emanoel Antonio <dev.emanoel@protonmail.com>
2023-11-20 17:45:18 -0300

    feat: add removeItem method

--- script.js
+++ script.js
@@ -6,24 +6,37 @@ let companies = [
   { company: 'Google', year:  1998 },
 ]
 
-const listAll = (companies) => {
-  for (let i = 0; i < companies.length; i++) {
-   console.log(`${i +1}. Empresa: ${companies[i].company}, Ano de Fundação: ${companies[i].year}`);
-  }
-}
-console.log('Lista das Empresas')
-listAll(companies)
+// const listAll = (companies) => {
+//   for (let i = 0; i < companies.length; i++) {
+//    console.log(`${i +1}. Empresa: ${companies[i].company}, Ano de Fundação: ${companies[i].year}`);
+//   }
+// }
+// console.log('Lista das Empresas')
+// listAll(companies)
 // const addItem = (companies) => {
-  
+//   for (let i = 0; i < companies.length; i++) {
+//     const element = companies[i];
+    
+//   }
 // }
 
 // const updateItem = (companies) => {
   
 // }
 
-// const removeItem = () => {
-  
-// }
+const removeItem = (companies) => {
+  let empresa = "OpenAI";
+  let novaListaDeEmpresas = [...companies]
+
+  for(let i = 0; i < novaListaDeEmpresas.length; i++){
+    if(novaListaDeEmpresas[i]["company"] == empresa){
+       novaListaDeEmpresas.splice(i, 1);
+    }
+  }
+  return novaListaDeEmpresas
+}
+console.log('Lista das Empresas', removeItem(companies))
+
 
 // const searchItemById = (id) => {
   
